4c1248fa7379dfe5f735597a3ac6a42e6e7405a7
BuildTools <unconfigured@null.spigotmc.org>
2022-04-18 14:35:16 -0400

    add AddAddress function

--- controllers/address.go
+++ controllers/address.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -17,7 +18,92 @@ import (
 *
 **/
 func AddAddress() gin.HandlerFunc {
-	
+	return func(c *gin.Context) {
+		user_id := c.Query("id")
+		if user_id == "" {
+			c.Header("Content-Type", "application/json")
+			c.JSON(http.StatusNotFound, gin.H{"error":"Invalid code"})
+			c.Abort()
+			return
+		}
+
+		address, err := ObjectIDFromHex(user_id)
+		if err != nil {
+			c.IndentedJSON(500, "Internal Server Error")
+		}
+
+		var addresses models.Address
+
+		addresses.Address_ID = primitive.NewObjectID()
+
+		if err = c.BindJSON(&address); err != nil {
+			c.IndentedJSON(http.StatusNotAcceptable, err.Error())
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		match_filter := bson.D{
+			Key: "$match", 
+			Value: bson.D{
+				primitive.E{
+					Key: "_id", 
+					Value: address
+				}
+			}
+		}
+		unwind := bson.D{
+			Key: "$unwind", 
+			Value: bson.D{
+				primitive.E{
+					Key: "path", 
+					Value: "$address",
+				},
+			},
+		}
+		group := bson.D{
+			Key: "$group", 
+			Value: bson.D{
+				primitive.E{
+					Key: "_id", 
+					Value: "$address_id",
+					}, 
+				Key: "count", 
+				Value: bson.D{
+					primitive.E{
+						Key: "$sum", 
+						Value: 1,
+					},
+				},
+			},
+		}
+		pointcursor, err := UserCollection.Aggregate(ctx, mongo.Pipeline{match_filter, unwind, group})
+		if err != nil {
+			c.IndentedJSON(500, "Internal Server Error")
+		}
+
+		var addressInfo []bson.M
+		if err = pointcursor.All(ctx, &addressInfo); err != nil {
+			panic(err)
+		} 
+
+		var size int32
+		for _, address_no := range addressInfo {
+			count := address_no["count"]
+			size := count.(int32)
+		}
+		if size < 2 {
+			filter := bson.D{primitive.E{Key: "_id", Value: address}}
+			update := bson.D{Key: "$push", Value: bson.D{primitive.E{Key: "address", Value: addresses}}}
+			_, err := UserCollection.UpdateOne(ctx, filter, update)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			c.IndentedJSON(400, "Not Allowed")
+		}
+		defer cancel()
+		ctx.Done()
+	}
 }
 
 /**
